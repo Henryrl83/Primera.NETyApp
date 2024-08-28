19d557386db33daab960a52c1097455fcf8d99f8
Henryrl83 <enriquerl83@gmail.com>
2024-08-28 19:08:14 +0200

    +Lista, Pila, Cola, Diccionario y metodo de extension

---
+++ PlayList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Primera.NETyApp
+{
+    public class PlayList<T, V, Z>
+    {
+
+    }
+    public class PlayList <T>
+    {
+
+        List<T> mediaList = new List<T>();
+        //ArrayList list = new ArrayList();
+        //T[] medialist = new T[] { };
+        public void Add(T media)
+        {
+            mediaList.Add(media);
+        }
+        public void PlayAll()
+        {
+            foreach(T media in mediaList)
+            {
+                Console.WriteLine(media);
+            }
+        }
+
+        public void Preview()
+        {
+            if (mediaList.Count > 0)
+            {
+                Console.WriteLine(mediaList[0]);
+            }
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,14 +22,73 @@ namespace Primera.NETyApp
             song2.Seconds = 500;
             song2.Position = new ShelvePosition(1, 0);
 
-            /*if (song.Title==song2.Title)
-            {
+            PlayList<Song> playListSongs = new PlayList<Song>();
+            playListSongs.Add(song);
+            playListSongs.Add(song2);
 
-            }*/
+            WriteLineSpecial(song, "Start With", "enjoy");
+
+
             Console.WriteLine(song2.ToString());
+
+            Tuple<int, string, Song> tuple = new Tuple<int, string, Song>(0, "Hola", song);
+
+            ListExplanation();
+            StackExplanation();
+            QueueExplanation();
+            DictionaryExplanation();
+            
+            //List<int> numbers = new List<int>();
+            //List<Song> songs = new List<Song>();
+            //List<int> ints = new List<int>();
+            //ArrayList list = new ArrayList();
 
             Console.ReadKey();
 
+        }
+        private static void ListExplanation()
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(0);
+            numbers.Add(1);
+            //numbers.Count
+        }
+        private static void StackExplanation()
+        {
+            Stack<Song> saleSongs = new Stack<Song>();
+            saleSongs.Push(new Song());
+            Song song = saleSongs.Pop();
+        }
+        private static void QueueExplanation()
+        {
+            Queue<int> queueNumbers = new Queue<int>();
+            queueNumbers.Enqueue(1);
+            int number = queueNumbers.Dequeue();
+        }
+        private static void DictionaryExplanation()
+        {
+            Dictionary<string, Client> clients = new Dictionary<string, Client>();
+            clients.Add("00000000D", new Client() { Name = "Enrique Raposo" });
+
+            if (!clients.ContainsKey("00000000D"))
+            {
+                clients.Add("00000000D", new Client() { Name = "Enrique Raposo" });
+            }
+
+            clients["00000000D"] = new Client();
+            Client myClient = clients["00000000D"];
+
+            var elements = new List<KeyValuePair<string, Client>>();
+            elements.Add(new KeyValuePair<string, Client>("0001", new Client()));
+            elements.Add(new KeyValuePair<string, Client>("0002", new Client()));
+
+
+            //Utils.AddRangeDictionary(clients, elements);
+            clients.AddRange(elements);
+        }
+        static void WriteLineSpecial<T>(T data, string prefix, string suffix)
+        {
+            Console.WriteLine($"{prefix} {data} {suffix}");
         }
         static void Declarations()
         {
